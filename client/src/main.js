9d81b0c0bbb1982bc940c6400f9c94daf563ab9e
Laaurent <48626779+Laaurent@users.noreply.github.com>
2022-02-24 17:35:15 +0100

    feature/add pinia

---
+++ client/src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from "vue";
+import "./tailwind.css";
+import App from "./App.vue";
+import { routes } from "./routes.js";
+import { createRouter, createWebHistory } from "vue-router";
+import { createPinia } from "pinia";
+
+const app = createApp(App);
+
+const router = createRouter({
+   history: createWebHistory(),
+   routes,
+});
+
+app.use(createPinia());
+app.use(router);
+app.mount("#app");
