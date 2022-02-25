3e1128ab80d704cc77f4898cca13ddc8f2f6e4c5
Florian PIAULT <florian.piault@mail-ecv.fr>
2022-02-25 10:30:33 +0100

    navbar begin

---
+++ client/src/components/layouts/NavbarComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="flex flex-row">
+    <ul class="flex-auto flex-col">
+      <li>Accueil</li>
+      <li>Séries</li>
+      <li>Nouveautés</li>
+      <li>Ma liste</li>
+    </ul>
+
+    <ul class="flex-none flex-col">
+      <li>
+        <label
+          ><IconsComponent icon="magnifyingGlass"></IconsComponent
+          ><input type="text"
+        /></label>
+      </li>
+      <li>dropdown profile</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import IconsComponent from "@/components/IconsComponent.vue";
+export default {
+  name: "NavbarComponent",
+  components: {
+    IconsComponent,
+  },
+};
+</script>
+
+<style></style>
