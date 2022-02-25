92214f0a8507453e0a8f6fde221523c67ad51b1d
Florian PIAULT <florian.piault@mail-ecv.fr>
2022-02-25 11:59:01 +0100

    navbar v1.1

--- client/src/components/layouts/NavbarComponent.vue
+++ client/src/components/layouts/NavbarComponent.vue
@@ -1,29 +1,78 @@
 <template>
-   <nav class="flex flex-row ">
-      <ul class="flex-auto flex-col">
-         <li>Accueil</li>
-         <li>Séries</li>
-         <li>Nouveautés</li>
-         <li>Ma liste</li>
-      </ul>
+  <nav class="flex items-center px-8 py-4">
+    <ul class="flex flex-auto flex-row items-center">
+      <!-- a changer debut -->
+      <li class="px-4 text-lg">
+        <span class="text-white-0">NOT</span>
+        <span class="text-red-600">NETFLIX</span>
+      </li>
+      <!-- a changer fin -->
+      <li class="px-2">Accueil</li>
+      <li class="px-2">Séries</li>
+      <li class="px-2">Nouveautés</li>
+      <li class="px-2">Ma liste</li>
+    </ul>
 
-      <ul class="flex-none flex-col">
-         <li>
-            <label><IconsComponent icon="magnifyingGlass"></IconsComponent><input type="text" /></label>
-         </li>
-         <li>dropdown profile</li>
-      </ul>
-   </nav>
+    <ul class="flex flex-none flex-row items-center">
+      <li>
+        <label
+          @click="displaySearchbar = true"
+          class="flex flex-row items-center"
+        >
+          <IconsComponent icon="magnifyingGlass"></IconsComponent>
+          <input
+            @blur="displaySearchbar = false"
+            class="text-white-100 mr-3 w-full appearance-none border-none bg-transparent py-1 px-2 leading-tight focus:outline-none"
+            :class="displaySearchbar ? searchbarMaxWidth : searchbarMinWidth"
+            v-model="textSearch"
+            type="text"
+          />
+        </label>
+      </li>
+      <li @click="displayProfileDropdown = !displayProfileDropdown">
+        1
+        <ul v-if="displayProfileDropdown">
+          <li>1.1</li>
+          <li>1.2</li>
+          <li>1.3</li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
 import IconsComponent from "@/components/IconsComponent.vue";
+import { ref } from "vue";
 export default {
-   name: "NavbarComponent",
-   components: {
-      IconsComponent,
-   },
+  name: "NavbarComponent",
+  components: {
+    IconsComponent,
+  },
+  setup() {
+    let displaySearchbar = ref(false);
+    let displayProfileDropdown = ref(false);
+    let textSearch = ref("");
+    let searchbarMinWidth = ref("searchbarMinWidth");
+    let searchbarMaxWidth = ref("searchbarMaxWidth");
+    return {
+      displaySearchbar,
+      displayProfileDropdown,
+      textSearch,
+      searchbarMinWidth,
+      searchbarMaxWidth,
+    };
+  },
 };
 </script>
 
-<style></style>
+<style>
+.searchbarMinWidth {
+  width: 0;
+  transition: 0.5s;
+}
+.searchbarMaxWidth {
+  width: 100%;
+  transition: 0.5s;
+}
+</style>
