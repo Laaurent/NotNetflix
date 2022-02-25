3e56305a8e65529340694ae6560aa29caff1462b
Laaurent <laurent-quenel@hotmail.Fr>
2022-02-25 10:30:59 +0100

    feature/footer without icons

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,27 +1,24 @@
 <script setup>
-import { ref } from 'vue'
+import FooterComponent from "./components/layouts/FooterComponent.vue";
+import { ref } from "vue";
 
-let counter = ref(0)
+let counter = ref(0);
 
 setInterval(() => {
-  counter.value++
-}, 1000)
+   counter.value++;
+}, 1000);
 </script>
 
 <template>
-  <div>
-    <header class="bg-white shadow" v-if="$route.meta.title">
-      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1
-          @click="counter = 0"
-          class="text-3xl font-bold leading-tight text-gray-900"
-        >
-          {{ $route.meta.title }} / {{ counter }}
-        </h1>
-      </div>
-    </header>
-    <main>
-      <router-view />
-    </main>
-  </div>
+   <div class="bg-slate-900">
+      <header class="bg-white shadow" v-if="$route.meta.title">
+         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 @click="counter = 0" class="text-3xl font-bold leading-tight text-gray-900">{{ $route.meta.title }} / {{ counter }}</h1>
+         </div>
+      </header>
+      <main>
+         <router-view />
+      </main>
+      <FooterComponent></FooterComponent>
+   </div>
 </template>
---
+++ client/src/components/layouts/FooterComponent.vue
@@ -0,0 +1,55 @@
+<template>
+   <div class="footer text-xs text-neutral-500 flex justify-center pt-6">
+      <div class="flex flex-col">
+         <div class="footer__networks flex py-2 text-white">
+            <div class="footer__networks_item">F</div>
+            <div class="footer__networks_item">I</div>
+            <div class="footer__networks_item">T</div>
+            <div class="footer__networks_item">Y</div>
+         </div>
+         <div class="footer__links flex gap-24 py-2 w-11/12">
+            <div class="footer__links_item">
+               <ul class="links__items py-2_col">
+                  <li class="items py-2"><a href="#">Audio et sous-titres</a></li>
+                  <li class="items py-2"><a href="#">Presse</a></li>
+                  <li class="items py-2"><a href="#">Confidentialité</a></li>
+                  <li class="items py-2"><a href="#">Nous contacter</a></li>
+               </ul>
+            </div>
+            <div class="footer__links_item">
+               <ul class="links__items py-2_col">
+                  <li class="items py-2"><a href="#">Audiodescription</a></li>
+                  <li class="items py-2"><a href="#">Relation invertisseurs</a></li>
+                  <li class="items py-2"><a href="#">Informations légales</a></li>
+               </ul>
+            </div>
+            <div class="footer__links_item">
+               <ul class="links__items py-2_col">
+                  <li class="items py-2"><a href="#">Centre d'aide</a></li>
+                  <li class="items py-2"><a href="#">Recrutement</a></li>
+                  <li class="items py-2"><a href="#">Préférence des cookies</a></li>
+               </ul>
+            </div>
+            <div class="footer__links_item">
+               <ul class="links__items py-2_col">
+                  <li class="items py-2"><a href="#">Carte cadeaux</a></li>
+                  <li class="items py-2"><a href="#">Conditions d'utilisation</a></li>
+                  <li class="items py-2"><a href="#">Mentions légales</a></li>
+               </ul>
+            </div>
+         </div>
+         <div class="footer__links_button py-2">
+            <button class="btn border-solid border-2 p-2 border-neutral-500">Code de service</button>
+         </div>
+         <div class="footer__links_copyright py-2">
+            <p>© 1997-2022 Netflix, Inc.{b0908601-6207-4061-8edc-e4d36e5c0dd9}</p>
+         </div>
+      </div>
+   </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
