788ba9c060e55c04e84d9123cca0126e72012fca
Florian PIAULT <florian.piault@mail-ecv.fr>
2022-02-25 14:51:15 +0100

    navbar finished

--- client/src/components/IconsComponent.vue
+++ client/src/components/IconsComponent.vue
@@ -1,6 +1,5 @@
 <template>
   <svg
-    :class="array_color[color]"
     v-if="icon === 'magnifyingGlass'"
     xmlns="http://www.w3.org/2000/svg"
     x="0px"
@@ -8,14 +7,14 @@
     width="24"
     height="24"
     viewBox="0 0 50 50"
-    style="fill: #000000"
+    :style="'fill:' + array_color[color]"
   >
     <path
       d="M 21 4 C 11.082241 4 3 12.082241 3 22 C 3 31.917759 11.082241 40 21 40 C 24.62177 40 27.99231 38.91393 30.820312 37.0625 L 43.378906 49.621094 L 47.621094 45.378906 L 35.224609 32.982422 C 37.581469 29.938384 39 26.13473 39 22 C 39 12.082241 30.917759 4 21 4 z M 21 8 C 28.756241 8 35 14.243759 35 22 C 35 29.756241 28.756241 36 21 36 C 13.243759 36 7 29.756241 7 22 C 7 14.243759 13.243759 8 21 8 z"
     ></path>
   </svg>
   <svg
-    :class="array_color[color]"
+    :style="array_color[color]"
     v-if="icon === 'instagram'"
     width="24"
     height="24"
@@ -29,7 +28,7 @@
     ></path>
   </svg>
   <svg
-    :class="array_color[color]"
+    :style="array_color[color]"
     v-if="icon === 'facebook'"
     width="24"
     height="24"
@@ -43,12 +42,11 @@
     ></path>
   </svg>
   <svg
-    :class="array_color[color]"
+    :style="'fill:' + array_color[color]"
     v-if="icon === 'youtube'"
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
@@ -57,12 +55,11 @@
     ></path>
   </svg>
   <svg
-    :class="array_color[color]"
+    :style="'fill:' + array_color[color]"
     v-if="icon === 'twitter'"
     width="24"
     height="24"
     viewBox="0 0 24 24"
-    fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
@@ -79,7 +76,7 @@ export default {
   data() {
     return {
       array_color: {
-        white: "fill-white",
+        white: "white",
       },
     };
   },
--- client/src/components/layouts/NavbarComponent.vue
+++ client/src/components/layouts/NavbarComponent.vue
@@ -17,9 +17,9 @@
       <li>
         <label
           @click="displaySearchbar = true"
-          class="flex flex-row items-center"
+          class="flex cursor-pointer flex-row items-center"
         >
-          <IconsComponent icon="magnifyingGlass"></IconsComponent>
+          <SearchIcon class="ml-2 h-5 w-5 text-white" />
           <input
             @blur="displaySearchbar = false"
             class="text-white-100 mr-3 w-full appearance-none border-none bg-transparent py-1 px-2 leading-tight focus:outline-none"
@@ -29,25 +29,57 @@
           />
         </label>
       </li>
-      <li @click="displayProfileDropdown = !displayProfileDropdown">
-        1
-        <ul v-if="displayProfileDropdown">
-          <li>1.1</li>
-          <li>1.2</li>
-          <li>1.3</li>
-        </ul>
+      <li>
+        <Popover class="relative">
+          <PopoverButton class="flex flex-row items-center">
+            <div
+              class="border-1 h-10 w-10 rounded-full border-white bg-center"
+              :style="{
+                backgroundImage: 'url(' + user.profilePic + ')',
+              }"
+            ></div>
+            <ChevronDownIcon
+              class="ml-2 h-5 w-5 text-white transition duration-150 ease-in-out group-hover:text-opacity-90"
+            />
+          </PopoverButton>
+
+          <PopoverPanel
+            class="w-128 absolute left-1/2 z-10 -translate-x-1/2 transform bg-neutral-900 px-2 py-2 text-sm text-white opacity-90"
+          >
+            <ul class="flex flex-auto flex-col">
+              <li class="hover:opacity:100 px-4 hover:bg-slate-700">
+                <router-link to="/about">Gérer les profils</router-link>
+              </li>
+              <li class="py-4"><hr /></li>
+              <li class="hover:opacity:100 px-4 hover:bg-slate-700">
+                <router-link to="/about">Compte</router-link>
+              </li>
+              <li class="hover:opacity:100 px-4 hover:bg-slate-700">
+                <router-link to="/about">Centre d'aide</router-link>
+              </li>
+              <li class="hover:opacity:100 px-4 hover:bg-slate-700">
+                <router-link to="/about">Se déconnecter</router-link>
+              </li>
+            </ul>
+          </PopoverPanel>
+        </Popover>
       </li>
     </ul>
   </nav>
 </template>
 
 <script>
-import IconsComponent from "@/components/IconsComponent.vue";
+import { ChevronDownIcon, SearchIcon } from "@heroicons/vue/solid";
+import { Popover, PopoverButton, PopoverPanel } from "@headlessui/vue";
 import { ref } from "vue";
 export default {
   name: "NavbarComponent",
   components: {
-    IconsComponent,
+    Popover,
+    PopoverButton,
+    PopoverPanel,
+    ChevronDownIcon,
+    SearchIcon,
   },
   setup() {
     let displaySearchbar = ref(false);
@@ -55,12 +87,17 @@ export default {
     let textSearch = ref("");
     let searchbarMinWidth = ref("searchbarMinWidth");
     let searchbarMaxWidth = ref("searchbarMaxWidth");
+
+    const user = {
+      profilePic: "https://via.placeholder.com/150",
+    };
     return {
       displaySearchbar,
       displayProfileDropdown,
       textSearch,
       searchbarMinWidth,
       searchbarMaxWidth,
+      user,
     };
   },
 };
