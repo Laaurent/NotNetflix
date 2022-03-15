fdfe2465f6b463374900808142c89c72cc431310
Soliman <Soliman.B15@gmail.com>
2022-03-15 20:07:59 +0100

    signup

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,14 @@
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import LogIn from './views/LogIn.vue'
+import SignUp from './views/SignUp.vue'
 import NotFound from './views/NotFound.vue'
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
   { path: '/', component: Home, meta: { title: 'Home' } },
   { path: '/login', component: LogIn, meta: { title: 'LogIn' } },
+  { path: '/signup', component: SignUp, meta: { title: 'SignUp' } },
   {
     path: '/about',
     meta: { title: 'About' },
